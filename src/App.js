aeb6b5a06a19e4c3422589d4fa7b56717987051b
Artem Zinchenko <lexart666@gmail.com>
2023-07-30 16:58:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <p>Artem Zinchenko</p>
+        <img src={logo} className="App-logo" alt="logo" />    
         <p>
           Artem Zinchenko         
         </p>        

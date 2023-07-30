42aaa8ccfa3c6d4ea18f956fd4131b2f40f32351
Artem Zinchenko <lexart666@gmail.com>
2023-07-30 16:53:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       <p>Artem Zinchenko</p>
         <p>
+          Artem Zinchenko
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
